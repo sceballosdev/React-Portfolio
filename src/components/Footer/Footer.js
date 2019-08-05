63a9f3fda9a374d3c72891d0b7af04cf20f77fda
Sergio Ramírez <esteban0513@hotmail.com>
2019-08-05 13:46:18 -0500

    Agregando footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,22 @@
 import React from 'react';
-
+import { Card, CardTitle, CardActions, Icon, Button } from 'react-mdl'
 
 function Footer(props) {
     return (
-        <div className="PageError">
-            <h3>Footer‼</h3>
-        </div>
+        <Card shadow={0} style={{ height: '256px', background: '#3E4EB8' }}>
+            <CardTitle expand style={{ alignItems: 'flex-start', color: '#fff' }}>
+                <h4 style={{ marginTop: '0' }}>
+                    Featured event:<br />
+                    May 24, 2016<br />
+                    7-11pm
+        </h4>
+            </CardTitle>
+            <CardActions border style={{ borderColor: 'rgba(255, 255, 255, 0.2)', display: 'flex', boxSizing: 'border-box', alignItems: 'center', color: '#fff' }}>
+                <Button colored style={{ color: '#fff' }}>Add to Calendar</Button>
+                <div className="mdl-layout-spacer"></div>
+                <Icon name="event" />
+            </CardActions>
+        </Card>
     );
 }
 
